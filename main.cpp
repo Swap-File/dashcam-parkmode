f602bdd1f398478e9ce35b0d5749ced818a4c138
swap-file <bigmarkslp@gmail.com>
2020-05-13 20:03:46 -0500

    Update main.cpp
    EEPROM config

--- main.cpp
+++ main.cpp
@@ -2,28 +2,31 @@
 #include <WiFi.h>
 #include <WiFiClientSecure.h>
 #include <driver/rtc_io.h>
+#include "EEPROM.h"
 
-// Home Wifi name & password
-const char *ssid = "";
-const char *password = "";
+#define INIT_VALUE 0xA1
 
-// Optiona IFTTT (email)
-String url = "";
-const char *host = "maker.ifttt.com";
-const int httpsPort = 443;
+// data that gets saved to eeprom
+typedef struct {
+  char init;
 
-// how many times to try connecting
-#define HOME_CONNECTION_ATTEMPTS 2
+  char ssid[32];
+  char password[32];
 
-// how many seconds to wait for a connection
-#define HOME_CONNECTION_TIME 20
+  char host[32];
+  char url[100];
+  int httpsPort;
 
-// how many minutes to delay before shutting down at home
-#define HOME_SHUTDOWN_DELAY 0.5
+  int home_connection_attempts; // retries
+  int home_connection_timeout;  // seconds
+  int home_shutdown_delay;      // minutes
+} EpromTags;
 
-// pins things are physically hooked up to (needs RTC support)
+EpromTags config;
+
+// pins things are physically hooked up to (pins must have RTC support)
 #define DASHCAM_PWR_OUTPUT_PIN GPIO_NUM_32
-#define DASHCAM_SHUTDOWN_DELAY_LED GPIO_NUM_13
+#define INDICATOR_LED GPIO_NUM_13
 #define CAR_PWR_INPUT_PIN GPIO_NUM_33
 
 // data stored in RTC to survive deep sleep
@@ -58,14 +61,14 @@ void send_email() {
 
   WiFiClientSecure client;
 
-  Serial.printf("Connecting to: %s\r\n", host);
-  if (!client.connect(host, httpsPort)) {
+  Serial.printf("Connecting to: %s\r\n", config.host);
+  if (!client.connect(config.host, config.httpsPort)) {
     Serial.printf("Connection failed\r\n");
     return;
   }
 
-  client.print(String("GET ") + url + " HTTP/1.1\r\n" + "Host: " + host +
-               "\r\n" + "Connection: close\r\n\r\n");
+  client.print(String("GET ") + config.url + " HTTP/1.1\r\n" +
+               "Host: " + config.host + "\r\n" + "Connection: close\r\n\r\n");
   Serial.printf("Request sent\r\n");
   while (client.connected()) {
     String line = client.readStringUntil('\n');
@@ -78,45 +81,152 @@ void send_email() {
   Serial.printf("HTTPS GET took: %lu ms\r\n", millis() - start_time);
 }
 
-void setup() {
+void print_config(void) {
+  Serial.printf("WiFi SSID: %s\r\n", config.ssid);
+  Serial.printf("WiFi Password: %s\r\n", config.password);
+  Serial.printf("Host: %s\r\n", config.host);
+  Serial.printf("URL: %s\r\n", config.url);
+  Serial.printf("Port %d\r\n", config.httpsPort);
+  Serial.printf("Home Connection Attemps (retries): %d\r\n",
+                config.home_connection_attempts);
+  Serial.printf("Home Connection Timeout (seconds): %d\r\n",
+                config.home_connection_timeout);
+  Serial.printf("Home Shutdown Delay (minutes): %d\r\n",
+                config.home_shutdown_delay);
+}
 
+int load_string(char *location, int size) {
+  int i = 0;
+  while (1) {
+    if (Serial.available() > 0) {
+      location[i] = Serial.read();
+      Serial.write(location[i]);
+      if (location[i] == '\n' || location[i] == '\r')
+        break;
+      if (i < size - 1 && isspace(location[i]) == false)
+        i++;
+    }
+  }
+  location[i] = '\0';
+  Serial.println("");
+  int temp;
+  sscanf(location, "%d", &temp);
+  return temp;
+}
+
+void first_boot() {
+  Serial.printf("Initial Powerup\r\n");
+  car_at_home = false;
+  car_off = false;
+
+  pinMode(INDICATOR_LED, OUTPUT);
+
+  uint32_t start_time = millis() + 10000;
+  while (config.init != INIT_VALUE || millis() < start_time) {
+    digitalWrite(INDICATOR_LED, 1);
+    delay(100);
+    digitalWrite(INDICATOR_LED, 0);
+    delay(100);
+    if (config.init != INIT_VALUE)
+      Serial.println("No configuration set.");
+    else
+      Serial.printf("Booting in %lu seconds...\r\n",
+                    (start_time - millis()) / 1000);
+
+    Serial.println("Press Any Key to Configure.");
+
+    if (Serial.peek() > 0) {
+      Serial.println("");
+      Serial.println("Starting Configuration");
+      digitalWrite(INDICATOR_LED, 1);
+      delay(100); // delay to capture any straggling characters
+      while (Serial.peek() > 0)
+        Serial.read(); // empty buffer
+
+      if (config.init == INIT_VALUE)
+        print_config();
+
+      Serial.println("");
+      Serial.println("Please enter new WiFi SSID:");
+      load_string(config.ssid, sizeof(config.ssid));
+
+      Serial.println("Please enter new WiFi Password:");
+      load_string(config.password, sizeof(config.password));
+
+      Serial.println(
+          "Please enter new ifttt host (enter for none)(maker.ifttt.com):");
+      load_string(config.host, sizeof(config.host));
+
+      Serial.println(
+          "Please enter new ifttt url (enter for "
+          "none)(/trigger/NAME/with/key/CODE):");
+      load_string(config.url, sizeof(config.url));
+
+      char temp[100];
+      Serial.println("Please enter new ifttt port (enter for none)(443):");
+      config.httpsPort = load_string(temp, sizeof(temp));
+
+      Serial.println("Please enter new Home Connection Attempts (Retries)(2):");
+      config.home_connection_attempts = load_string(temp, sizeof(temp));
+
+      Serial.println("Please enter new Home Connection Timeout (Seconds)(20):");
+      config.home_connection_timeout = load_string(temp, sizeof(temp));
+
+      Serial.println("Please enter new Home Shutdown Delay (Minutes)(1):");
+      config.home_shutdown_delay = load_string(temp, sizeof(temp));
+
+      config.init = INIT_VALUE;
+      EEPROM.put(0, config);
+
+      Serial.println("");
+      print_config();
+      Serial.println("");
+
+      Serial.println("Press enter to continue:");
+      load_string(temp, sizeof(temp));
+
+      EEPROM.commit();
+    }
+  }
+  digitalWrite(INDICATOR_LED, 0);
+}
+
+void setup() {
   Serial.begin(115200);
+  EEPROM.begin(sizeof(EpromTags));
+  EEPROM.get(0, config);
 
   esp_sleep_wakeup_cause_t wakeup_reason = esp_sleep_get_wakeup_cause();
 
   if (wakeup_reason == ESP_SLEEP_WAKEUP_EXT1) {
-
     car_off = !car_off;
 
     if (car_off) {
-
       // clear at home flag
+      Serial.printf("Car stopped, checking for Home AP....\r\n");
       car_at_home = false;
 
-      Serial.printf("Car stopped, checking for Home AP....\r\n");
-
-      for (int j = 0; j < HOME_CONNECTION_ATTEMPTS; j++) {
-
+      for (int j = 0; j < config.home_connection_attempts; j++) {
         Serial.printf("Starting Scan #%d\r\n", j);
-
         WiFi.mode(WIFI_STA);
-        WiFi.begin(ssid, password);
-
-        unsigned long wifi_timeout = millis() + HOME_CONNECTION_TIME * 1000;
-
+        WiFi.begin(config.ssid, config.password);
+        unsigned long wifi_timeout =
+            millis() + config.home_connection_timeout * 1000;
         while (millis() < wifi_timeout) {
           if (WiFi.status() == WL_CONNECTED) {
             car_at_home = true;
-            Serial.printf("WiFi Connected in: %lu ms\r\n", wifi_timeout - millis());
+            Serial.printf("WiFi Connected in: %lu ms\r\n",
+                          wifi_timeout - millis());
             break;
           }
-          delay(100);
         }
-        if (car_at_home)
+
+        if (car_at_home && strlen(config.url) > 0)
           send_email();
 
         WiFi.disconnect();
         WiFi.mode(WIFI_OFF);
+
         // don't scan again if we are already home
         if (car_at_home)
           break;
@@ -124,43 +234,36 @@ void setup() {
 
       if (!car_at_home)
         Serial.printf("Leaving dashcam on...\r\n");
-
     } else {
       Serial.printf("Car turned on...\r\n");
     }
 
   } else if (wakeup_reason == ESP_SLEEP_WAKEUP_TIMER) {
-
     // turn the camera off and clear the flag
     hold_pin_off(DASHCAM_PWR_OUTPUT_PIN);
-    hold_pin_off(DASHCAM_SHUTDOWN_DELAY_LED);
+    hold_pin_off(INDICATOR_LED);
     car_at_home = false;
-
   } else {
-
-    // ext1 wakeup will immediately trigger if we assume wrong
-    Serial.printf("Initial Powerup\r\n");
-    car_at_home = false;
-    car_off = false;
+    first_boot();
   }
 
   Serial.flush();
-
   const uint64_t mask = 1ULL << CAR_PWR_INPUT_PIN;
 
   if (car_off) {
     // car is off, watch for it to turn on
     esp_sleep_enable_ext1_wakeup(mask, ESP_EXT1_WAKEUP_ALL_LOW);
-
     // car is off, if we are home set a timer to shut the dashcam off
     if (car_at_home) {
-      Serial.printf("Dashcam shutdown in %f minutes.\r\n", HOME_SHUTDOWN_DELAY);
-      hold_pin_on(DASHCAM_SHUTDOWN_DELAY_LED);
-      esp_sleep_enable_timer_wakeup(HOME_SHUTDOWN_DELAY * 60 * 1000 * 1000);
+      Serial.printf("Dashcam shutdown in %d minutes.\r\n",
+                    config.home_shutdown_delay);
+      hold_pin_on(INDICATOR_LED);
+      esp_sleep_enable_timer_wakeup(config.home_shutdown_delay * 60 * 1000 *
+                                    1000);
     }
   } else {
     // car is on, watch for it to turn off
-    hold_pin_off(DASHCAM_SHUTDOWN_DELAY_LED);
+    hold_pin_off(INDICATOR_LED);
     hold_pin_on(DASHCAM_PWR_OUTPUT_PIN);
     esp_sleep_enable_ext1_wakeup(mask, ESP_EXT1_WAKEUP_ANY_HIGH);
   }
